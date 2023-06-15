ef8a709f4afdda3f4b8334b0a8d1622911d81107
Rahman Kukuh <rahmankukuh45@gmail.com>
2023-06-16 01:27:44 +0700

    minor changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient()
@@ -12,6 +11,7 @@ const port = 5000;
 
 
 app.listen(port, () => {
+
     console.log(`Server is running on port ${port}`);
 });
 
@@ -21,109 +21,109 @@ app.use('/user', userRoutes);
 
 
 
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  
-  try {
-      // Find the user by username
-      const user = await prisma.user.findUnique({ where: { username } });
-      
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      
-      // Compare the provided password with the stored hashed password
-      
-      
-      if (password !== user.password) {
-        return res.status(401).json({ message: 'Invalid password' });
-      }
-  
-      // Generate a JWT token
-      const token = jwt.sign({ userId: user.id }, 'secret');
-      
-      res.status(200).json({
-        status: 'success',
-        message: 'Login successful',
-        data: {
-          user: user,
-          token: token,
-        },
-      });
-    } catch (error) {
-      console.error('Error logging in:', error);
-      res.status(500).json({ message: 'Failed to login' });
-    }
-  })
-  
-  app.post('/register', async (req, res) => {
-    const { username, name, email, password } = req.body;
-    
-    try {
-      // Check if the username is already used
-      const existingUser = await prisma.user.findUnique({
-        where: {
-          username: username,
-        },
-      });
-      
-      if (existingUser) {
-        return res.status(409).json({ message: 'Username is already taken' });
-      }
-      
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      // Create a new user
-      const newUser = await prisma.user.create({
-        data: {
-          username,
-          name,
-          email,
-          password: hashedPassword,
-        },
-      });
-      
-      // Generate JWT token
-      const token = jwt.sign({ userId: newUser.id }, process.env.JWT_ACCESS_SECRET);
-      
-      res.status(201).json({
-        status: 'success',
-        message: 'Successfully registered',
-        data: {
-          user: newUser,
-          token: token,
-        },
-      });
-    } catch (error) {
-      console.error('Error registering user:', error);
-      res.status(500).json({ message: 'Failed to register user' });
-    }
-  });
-  
- 
+app.post('/login', async(req, res) => {
+    const { username, password } = req.body;
 
-  // const login = async (req, res) => {
-    //     const { username, password } = req.body;
-  
-    //     try {
-      //       // Find the user by username
-      //       const user = await prisma.user.findUnique({ where: { username } });
-      
-      //       if (!user) {
-        //         return res.status(404).json({ message: 'User not found' });
-        //       }
-        
+    try {
+        // Find the user by username
+        const user = await prisma.user.findUnique({ where: { username } });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Compare the provided password with the stored hashed password
+
+
+        if (password !== user.password) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        // Generate a JWT token
+        const token = jwt.sign({ userId: user.id }, 'secret');
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Login successful',
+            data: {
+                user: user,
+                token: token,
+            },
+        });
+    } catch (error) {
+        console.error('Error logging in:', error);
+        res.status(500).json({ message: 'Failed to login' });
+    }
+})
+
+app.post('/register', async(req, res) => {
+    const { username, name, email, password } = req.body;
+
+    try {
+        // Check if the username is already used
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                username: username,
+            },
+        });
+
+        if (existingUser) {
+            return res.status(409).json({ message: 'Username is already taken' });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                name,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        // Generate JWT token
+        const token = jwt.sign({ userId: newUser.id }, process.env.JWT_ACCESS_SECRET);
+
+        res.status(201).json({
+            status: 'success',
+            message: 'Successfully registered',
+            data: {
+                user: newUser,
+                token: token,
+            },
+        });
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({ message: 'Failed to register user' });
+    }
+});
+
+
+
+// const login = async (req, res) => {
+//     const { username, password } = req.body;
+
+//     try {
+//       // Find the user by username
+//       const user = await prisma.user.findUnique({ where: { username } });
+
+//       if (!user) {
+//         return res.status(404).json({ message: 'User not found' });
+//       }
+
 //       // Compare the provided password with the stored hashed password
 //       const isPasswordValid = await bcrypt.compare(password, user.password);
-  
+
 //       if (!isPasswordValid) {
 //         return res.status(401).json({ message: 'Invalid password' });
 //       }
-  
+
 //       // Generate a JWT token
 //       const token = jwt.sign({ userId: user.id }, 'your-secret-key');
-  
+
 //       res.status(200).json({
 //         status: 'success',
 //         message: 'Login successful',
@@ -137,7 +137,6 @@ app.post('/login', async (req, res) => {
 //       res.status(500).json({ message: 'Failed to login' });
 //     }
 //   };
-  
 
 
 
@@ -148,7 +147,18 @@ app.post('/login', async (req, res) => {
 
 
 
-app.get('/', async (req, res, next) => {
-    res.status(200).send({ message: "Cool It Works!"});
+
+app.get('/', async(req, res, next) => {
+    const checkDatabaseConnection = async() => {
+        try {
+            prisma.$connect();
+            console.log('Connected to the database.');
+        } catch (error) {
+            console.error('Failed to connect to the database:', error);
+        } finally {
+            prisma.$disconnect();
+        }
+    }
+    checkDatabaseConnection();
+    res.status(200).send({ message: "Cool It Works!" });
 });
-
--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,87 +1,86 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient()
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
 const verifyJwt = promisify(jwt.verify);
 
-const getAllUser = async (req, res) => {
-  const token = req.headers.authorization;
-  if (!token) {
-    res.status(401).json({ error: 'No authorization token provided' });
-    return;
-  }
-
-  try {
-    const splitToken = token.split(" ")[1];
-    jwt.verify(splitToken, 'secret');
-    const users = await prisma.user.findMany({
-      include: {
-        posts: {
-          include: {
-            design_type: true,
-            design_style: true,
-          }
-        }
-      },
-      select: {
-        username: true,
-        email: true,
-        posts: true,
-      },
-    });
-    res.json({
-      status: 'success',
-      message: 'Successfully getAllUsers',
-      data: users,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({
-      status: 'failed',
-      message: 'Service getAllUser unavailable.',
-      error: error.message,
-    });
-  }
-}
-    
-const getUserInfo = async (req, res) => {
+const getAllUser = async(req, res) => {
     const token = req.headers.authorization;
     if (!token) {
-      res.status(401).json({ error: 'No authorization token provided' });
-    } else {
-      try {
+        res.status(401).json({ error: 'No authorization token provided' });
+        return;
+    }
+
+    try {
         const splitToken = token.split(" ")[1];
         jwt.verify(splitToken, 'secret');
-        const username = req.params.username;
-        const userInfo = await prisma.user.findUnique({
-          where: { username: username },
-          select: {
-            username: true,
-            name: true,
-            email: true,
-            posts: true,
-          },
+        const users = await prisma.user.findMany({
+            include: {
+                posts: {
+                    include: {
+                        design_type: true,
+                        design_style: true,
+                    }
+                }
+            },
+            select: {
+                username: true,
+                email: true,
+                posts: true,
+            },
         });
-        
-        if (userInfo && userInfo.username !== username) {
-          return res.status(400).json({ error: "Username not available" });
-        }
-        res.status(200).json({
-          status: 'success',
-          message: 'Successfully getUserInfo',
-          data: userInfo,
+        res.json({
+            status: 'success',
+            message: 'Successfully getAllUsers',
+            data: users,
         });
-      } catch (error) {
+    } catch (error) {
         console.log(error.message);
         res.status(500).json({
-          status: 'failed',
-          message: 'Service getUserInfo unavailable.',
+            status: 'failed',
+            message: 'Service getAllUser unavailable.',
+            error: error.message,
         });
-      }
     }
-  };
-  
+}
+
+const getUserInfo = async(req, res) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        res.status(401).json({ error: 'No authorization token provided' });
+    } else {
+        try {
+            const splitToken = token.split(" ")[1];
+            jwt.verify(splitToken, 'secret');
+            const username = req.params.username;
+            const userInfo = await prisma.user.findUnique({
+                where: { username: username },
+                select: {
+                    username: true,
+                    name: true,
+                    email: true,
+                    posts: true,
+                },
+            });
+
+            if (userInfo && userInfo.username !== username) {
+                return res.status(400).json({ error: "Username not available" });
+            }
+            res.status(200).json({
+                status: 'success',
+                message: 'Successfully getUserInfo',
+                data: userInfo,
+            });
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({
+                status: 'failed',
+                message: 'Service getUserInfo unavailable.',
+            });
+        }
+    }
+};
+
 
 // const register = async (req, res) => {
 //     const {username, name, email, password} = req.body;
