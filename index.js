0618866fe6a137e5db1f9884b3efd42da58aa5e2
rakuma45 <rahmankukuh45@gmail.com>
2023-06-15 20:29:41 +0700

    change port to 8080

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient()
 const userRoutes = require('./src/routes/users.routes');
 const postRoutes = require('./src/routes/posts.routes');
-const port = 5000;
+const port = 8080;
 
 
 
@@ -100,53 +100,6 @@ app.post('/register', async(req, res) => {
         res.status(500).json({ message: 'Failed to register user' });
     }
 });
-
-
-
-// const login = async (req, res) => {
-//     const { username, password } = req.body;
-
-//     try {
-//       // Find the user by username
-//       const user = await prisma.user.findUnique({ where: { username } });
-
-//       if (!user) {
-//         return res.status(404).json({ message: 'User not found' });
-//       }
-
-//       // Compare the provided password with the stored hashed password
-//       const isPasswordValid = await bcrypt.compare(password, user.password);
-
-//       if (!isPasswordValid) {
-//         return res.status(401).json({ message: 'Invalid password' });
-//       }
-
-//       // Generate a JWT token
-//       const token = jwt.sign({ userId: user.id }, 'your-secret-key');
-
-//       res.status(200).json({
-//         status: 'success',
-//         message: 'Login successful',
-//         data: {
-//           user: user,
-//           token: token,
-//         },
-//       });
-//     } catch (error) {
-//       console.error('Error logging in:', error);
-//       res.status(500).json({ message: 'Failed to login' });
-//     }
-//   };
-
-
-
-
-
-
-
-
-
-
 
 app.get('/', async(req, res, next) => {
     const checkDatabaseConnection = async() => {
